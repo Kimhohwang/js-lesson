037fbc8498b87a7f26d8b362aab0f695cbdc3858
Kimhohwang <fodric@ontactkorea.net>
2024-07-16 17:05:50 +0900

    연습중 commit

---
+++ 연습/practice.js
@@ -0,0 +1,15 @@
+let 변수1 = 5000;
+console.log(변수1);
+
+let 변수2 = 5000;
+console.log(변수1 + 변수2);
+
+let 변수3 = "5000";
+
+console.log(변수1 + 변수3);
+console.log("변수1 :" + 변수1);
+
+console.log(변수1 + 변수3 + 변수2);
+
+const 변수4 = 변수1 + 변수2;
+console.log(변수4);
